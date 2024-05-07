263c2ea4677a54f963b89392ebcb7fcf9e88f49c
debertjamie <debertjamie@outlook.com>
2024-05-07 23:32:00 +0700

    feat: Extra parts for large media

--- ui/landing/photo.tsx
+++ ui/landing/photo.tsx
@@ -2,16 +2,23 @@ import Image from "next/image";
 
 export function Photo() {
   return (
-    <div className="space-y-1">
+    <div className="space-y-1 lg:flex lg:gap-x-6">
       <Image
         src="/DebertJamie.jpg"
         alt="Debert Jamie"
-        width={395}
-        height={395}
-        className="rounded-xl bg-secondary"
+        width={0}
+        height={0}
+        sizes="100%"
+        className="rounded-xl bg-secondary w-full h-full lg:rounded-full lg:w-20"
         priority={true}
       ></Image>
-      <p className="text-base lg:text-center">A photo of me 📸</p>
+      <p className="text-base lg:hidden">A photo of me 📸</p>
+      <div className="hidden lg:flex lg:flex-col lg:justify-center">
+        <h3 className="text-xl font-semibold text-accent">@DebertJamieChanderson</h3>
+        <div className="flex flex-wrap text-sm">
+          <p>High Schooler, Espresso Addict, Math Maven, Tech Admirer</p>
+        </div>
+      </div>
     </div>
   );
 }
