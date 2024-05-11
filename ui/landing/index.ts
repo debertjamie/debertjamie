dde8454771755b0cab25b612836631a8355bd7ff
debertjamie <debertjamie@outlook.com>
2024-05-11 15:06:34 +0700

    feat: Redesign landing page

--- ui/landing/index.ts
+++ ui/landing/index.ts
@@ -3,3 +3,7 @@ export * from "./extras";
 export * from "./photo";
 export * from "./time";
 export * from "./contact";
+
+export * from "./head";
+export * from "./socials";
+export * from "./status";
