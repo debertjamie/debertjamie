8bb9ac46ec5b0b8d657ce09e55681af968683f33
debertjamie <debertjamie@outlook.com>
2024-05-12 15:49:55 +0700

    feat: Add mobile version

--- wrapper/darkmode.tsx
+++ wrapper/darkmode.tsx
@@ -15,15 +15,32 @@ export function ThemedHTML({ children }: { children: React.ReactNode }) {
 
 export function ToggleTheme() {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
-  console.log(isDarkMode);
 
   return (
-    <div className="*:cursor-pointer">
+    <div className="*:cursor-pointer" onClick={toggleDarkMode}>
       {isDarkMode ? (
-        <DarkTheme width={18} height={18} onClick={toggleDarkMode} />
+        <DarkTheme width={18} height={18} />
       ) : (
-        <LightTheme width={18} height={18} onClick={toggleDarkMode} />
+        <LightTheme width={18} height={18} />
       )}
     </div>
+  );
+}
+
+export function MobileToggleTheme() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+
+  return (
+    <p
+      className="*:cursor-pointer flex gap-x-2 items-center pt-2 border-t-2 border-t-brand-950 dark:border-t-brand-50"
+      onClick={toggleDarkMode}
+    >
+      {isDarkMode ? (
+        <DarkTheme width={28} height={28} className="" />
+      ) : (
+        <LightTheme width={28} height={28} className="" />
+      )}
+      Switch Theme
+    </p>
   );
 }
