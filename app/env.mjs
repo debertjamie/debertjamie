27bccc7ff4beba8fdb728cbab3c8928876fa60c5
debertjamie <debertjamie@outlook.com>
2024-05-18 19:49:06 +0700

    refactor: ENV variables

--- app/env.mjs
+++ app/env.mjs
@@ -1,1 +1,12 @@
 export const publicUrl = `https://${process.env.NODE_ENV === "development" ? process.env.VERCEL_URL : process.env.NEXT_PUBLIC_VERCEL_URL}`;
+
+// DATABASE (POSTGRES)
+export const dbUrl = process.env.DB_URL;
+
+// AUTHENTICATION
+export const discordId = process.env.DISCORD_CLIENT_ID;
+export const discordSecret = process.env.DISCORD_CLIENT_SECRET;
+
+// ADMIN EMAIL(S)
+const email = process.env.ADMIN_EMAIL;
+export const adminEmail = email.includes(",") ? email.split(",") : email;
