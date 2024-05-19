90a9044f2740c01c27f5163affd03bb9dc5d4fc6
debertjamie <debertjamie@outlook.com>
2024-05-19 08:12:02 +0700

    style: Remove unused colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,23 +9,7 @@ const config: Config = {
   ],
   darkMode: "selector",
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          50: "#E1F2FF",
-          100: "#C8E7FE",
-          200: "#90CEFD",
-          300: "#59B6FD",
-          400: "#219CFC",
-          500: "#0387EC",
-          600: "#036BBA",
-          700: "#02518D",
-          800: "#013760",
-          900: "#011A2D",
-          950: "#000E19",
-        },
-      },
-    },
+    extend: {},
     screens: {
       xs: "475px",
       ...defaultTheme.screens,
