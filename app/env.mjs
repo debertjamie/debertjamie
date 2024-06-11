748c2da9196af54216227bb69beb1cf64f858e28
debertjamie <debertjamie@outlook.com>
2024-06-11 13:14:43 +0700

    chore: ENV variables

--- app/env.mjs
+++ app/env.mjs
@@ -13,5 +13,9 @@ const email = process.env.ADMIN_EMAIL;
 export const adminEmail = email.includes(",") ? email.split(",") : email;
 
 // GIT COMMIT SHA
-// Defaults to first commit
 export const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? "9797610087981a0db4e1f6134a4f718501ba55c6";
+
+// SPOTIFY
+export const spotifyId = process.env.SPOTIFY_CLIENT_ID
+export const spotifySecret = process.env.SPOTIFY_CLIENT_SECRET;
+export const spotifyToken = process.env.SPOTIFY_REFRESH_TOKEN;
