523dae0ae3fac860f38934601da42ebffbc40924
debertjamie <debertjamie@outlook.com>
2024-06-22 15:40:26 +0700

    feat: Change / layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
-import { Head, More } from "@/ui/landing";
+import { Head, Intro, More } from "@/ui/landing";
 
 export default function Home() {
   return (
-    <>
-      <main className="space-y-8">
-        <Head />
-        <More />
-      </main>
-    </>
+    <main className="space-y-16 mt-8 sm:mt-28">
+      <Head />
+      <div className="sm:py-24" />
+      <Intro />
+      <More />
+    </main>
   );
 }
 
-const title = "@Debert Jamie C";
+const title = "Debert Jamie C";
 const description =
   "Hey! I'm Debert, a Computer Science enthusiast who likes Maths and Tech ⌨️";
 
