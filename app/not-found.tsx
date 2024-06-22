8f15021b7cb2215cb3a57ffa84cf9fb5f6c8c254
debertjamie <debertjamie@outlook.com>
2024-06-22 15:39:48 +0700

    feat: Change 404 layout

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,17 +2,17 @@ import Link from "next/link";
 
 export default async function NotFound() {
   return (
-    <div className="mx-auto max-w-2xl space-y-10">
+    <div className="h-screen flex flex-col justify-center items-center gap-y-6">
       <h1 className="text-5xl font-bold">404 Page Not Found</h1>
-      <p className="text-lg">What are you even looking for 🤨?</p>
-      <p className="text-lg">
-        This place doesn&#39;t hold any secrets... yet. Check back later.
-      </p>
+      <div className="text-lg">
+        <p>What are you even looking for 🤨?</p>
+        <p>This place doesn&#39;t hold any secrets... yet. Check back later.</p>
+      </div>
       <Link
-        className="block w-fit mt-12 px-4 py-2 bg-blue-800 text-zinc-100 font-semibold text-lg rounded-lg"
+        className="block w-fit px-4 py-2 bg-blue-800 text-zinc-100 font-semibold text-lg rounded-lg"
         href="/"
       >
-        Return Home -&gt;
+        Return Home
       </Link>
     </div>
   );
