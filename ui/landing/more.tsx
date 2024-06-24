8161af2254119e694885b35f4225fadb749d8a75
debertjamie <debertjamie@outlook.com>
2024-06-24 12:33:41 +0700

    feat: New button to the grid

--- ui/landing/more.tsx
+++ ui/landing/more.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { Lanyard, Spotify, Socials } from ".";
-import { DownloadIcon } from "../icons";
+import { DownloadIcon, BooksIcon } from "../icons";
 
 export function More() {
   return (
@@ -16,9 +16,13 @@ export function More() {
           <Lanyard />
         </Link>
         <div className="bg-neutral-800 text-zinc-100 rounded-2xl px-4 py-2 flex justify-center items-center *:inline">
-          <p className="text-lg">Download Resume</p>
-          <DownloadIcon className="w-4 ml-2" />
+          <p className="text-xl">Download Resume</p>
+          <DownloadIcon className="w-5 ml-2" />
         </div>
+        <Link href="/guestbook" className="bg-slate-800 border-slate-600 border-2 text-zinc-100 rounded-lg px-4 py-2 flex justify-center items-center *:inline hover:scale-95 duration-300">
+          <BooksIcon className="w-8 ml-2" />
+          <p className="text-2xl">Guestbook</p>
+        </Link>
       </div>
       <div className="md:col-span-2">
         <Socials />
