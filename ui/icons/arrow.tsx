69ae9227109e00af87d0997915e10e5069f057d4
debertjamie <debertjamie@outlook.com>
2024-07-06 11:52:46 +0700

    feat: Add icon

--- ui/icons/arrow.tsx
+++ ui/icons/arrow.tsx
@@ -60,3 +60,23 @@ export function DownloadIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function DoubleArrowIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="m6 17 5-5-5-5" />
+      <path d="m13 17 5-5-5-5" />
+    </svg>
+  );
+}
