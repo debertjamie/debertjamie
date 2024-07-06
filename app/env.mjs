01e3e261667f5332557cb95f9199f20c4a9d9e42
debertjamie <debertjamie@outlook.com>
2024-07-06 11:44:37 +0700

    refactor: Format code

--- app/env.mjs
+++ app/env.mjs
@@ -13,10 +13,12 @@ const email = process.env.ADMIN_EMAIL;
 export const adminEmail = email.includes(",") ? email.split(",") : email;
 
 // GIT COMMIT SHA
-export const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? "9797610087981a0db4e1f6134a4f718501ba55c6";
+export const sha =
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ??
+  "9797610087981a0db4e1f6134a4f718501ba55c6";
 
 // SPOTIFY
-export const spotifyId = process.env.SPOTIFY_CLIENT_ID
+export const spotifyId = process.env.SPOTIFY_CLIENT_ID;
 export const spotifySecret = process.env.SPOTIFY_CLIENT_SECRET;
 export const spotifyToken = process.env.SPOTIFY_REFRESH_TOKEN;
 
