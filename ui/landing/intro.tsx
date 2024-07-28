beec61d52c71f807b19e3f56d080ab9795d8b8e0
debertjamie <debertjamie@outlook.com>
2024-07-28 18:45:10 +0700

    feat: Change intro in landing page

--- ui/landing/intro.tsx
+++ ui/landing/intro.tsx
@@ -6,10 +6,10 @@ export function Intro() {
       <h2 className="font-semibold">A Little Intro About Me</h2>
       <p className="text-justify">
         Hi! I'm Debert, a high school graduate from Indonesia. I currently live
-        in North Sumatra and enjoys my free time coding or just listening to
-        music. I started learning code during the 2020 pandemic, with
-        documentation pages and StackOverflow being my main teacher. Currently
-        preparing for my next step of life: university.
+        in Special Region of Yogyakarta and enjoys my free time coding or just
+        listening to music. I started learning code during the 2020 pandemic,
+        with documentation pages and StackOverflow being my main teacher.
+        Currently a freshman in the University of Gadjah Mada.
       </p>
       <Link
         href="/about"
