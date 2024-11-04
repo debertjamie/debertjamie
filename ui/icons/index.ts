5550fe4393d924e69c4c5562fec63adb486595cf
debertjamie <debertjamie@outlook.com>
2024-11-04 22:23:18 +0700

    core: Remove unused SVG icons

--- ui/icons/index.ts
+++ ui/icons/index.ts
@@ -1,11 +1,5 @@
 export * from "./theme";
 export * from "./logo";
-
-export * from "./lang";
 export * from "./arrow";
-export * from "./programming"
-export * from "./webdev";
 export * from "./socials";
-export * from "./software";
-export * from "./cloud";
 export * from "./misc";
