b664a3f35d1430271c4221ebedd0b5d1f7d4dc3b
debertjamie <debertjamie@outlook.com>
2024-11-08 23:11:19 +0700

    feat: Add icon to header

--- ui/header/header.tsx
+++ ui/header/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 import { ToggleTheme } from ".";
 import { HamburgerMenu, Menu } from ".";
@@ -21,7 +21,10 @@ export function Header() {
     <header className="z-50 sticky top-8">
       {isWide ? (
         <div className="flex flex-wrap items-center justify-between gap-x-6 h-14 shadow-md dark:shadow-zinc-800 bg-zinc-100 dark:bg-zinc-950 border border-zinc-400 dark:border-zinc-600 rounded-lg py-2 px-4 text-xl">
-          <p className="font-medium">Debert Jamie</p>
+          <div className="flex gap-x-4 items-center">
+            <Image src="/static/icon.png" alt="Logo" height={0} width={0} sizes="100%" className="h-10 w-10 rounded-full dark:invert"/>
+            <p className="font-semibold">Debert Jamie</p>
+          </div>
           <div className="flex items-center gap-x-6">
             <Menu name="Home" path="/" />
             <Menu name="About" path="/about" />
