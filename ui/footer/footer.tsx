ce3786b94901643db08587c54074b412521dadb1
debertjamie <debertjamie@outlook.com>
2024-11-09 00:08:02 +0700

    fix: Fix mobile view

--- ui/footer/footer.tsx
+++ ui/footer/footer.tsx
@@ -5,10 +5,10 @@ export function Footer() {
   return (
     <footer className="mb-1 lg:mb-4 pt-4 border-t-2 border-t-zinc-950 space-y-4 dark:border-t-zinc-100">
       <div className="text-base flex justify-center">
-        <p className="after:content-['\00B7'] after:mx-2">&#169; 2024 Debert Jamie Chanderson</p>
+        <p className="md:after:content-['\00B7'] md:after:mx-2">&#169; 2024 Debert Jamie Chanderson</p>
         <Link 
           href={`https://github.com/debertjamie/debertjamie/commit/${sha}`}
-          className="text-sky-700 dark:text-sky-400"
+          className="hidden md:inline text-sky-700 dark:text-sky-400"
         >{sha.slice(0, 7)}</Link>
       </div>
     </footer>
--- ui/landing/head.tsx
+++ ui/landing/head.tsx
@@ -6,12 +6,12 @@ import { Time } from "../components";
 export function Head() {
   return (
     <section className="space-y-16">
-      <div className="space-y-14">
+      <div className="space-y-6 sm:space-y-14">
         <h1 className="text-4xl sm:text-6xl font-bold">
           <p className="text-lg font-normal">Hello, I'm</p>
           <Typewriter cursor words={["Debert Jamie Chanderson"]} />
         </h1>
-        <h2 className="text-3xl">
+        <h2 className="text-lg sm:text-3xl">
           Information engineering undergraduate at Gadjah Mada University
         </h2>
       </div>
