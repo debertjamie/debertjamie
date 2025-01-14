b8b7b0276ecda71a94759062428eeb720ecf9d95
debertjamie <debertjamie@outlook.com>
2025-01-14 21:44:46 +0700

    Happy New Year!

--- ui/footer/footer.tsx
+++ ui/footer/footer.tsx
@@ -5,11 +5,11 @@ export function Footer() {
   return (
     <footer className="mb-1 lg:mb-4 pt-4 border-t-2 border-t-zinc-950 space-y-4 dark:border-t-zinc-100">
       <div className="text-base flex justify-center">
-        <p className="md:after:content-['\00B7'] md:after:mx-2">&#169; 2024 Debert Jamie Chanderson</p>
+        <p className="md:after:content-['\00B7'] md:after:mx-2">&#169; 2025 Debert Jamie Chanderson</p>
         <Link 
           href={`https://github.com/debertjamie/debertjamie/commit/${sha}`}
           className="hidden md:inline text-sky-700 dark:text-sky-400"
-        >{sha.slice(0, 7)}</Link>
+        >{sha.slice(0, 7)} Edition</Link>
       </div>
     </footer>
   );
