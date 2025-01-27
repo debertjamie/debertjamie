eb0e2532051c2f3c491ad602743f8741f8e76e7f
debertjamie <debertjamie@outlook.com>
2025-01-27 16:19:27 +0700

    feat: Link URL Icon

--- ui/icons/arrow.tsx
+++ ui/icons/arrow.tsx
@@ -19,3 +19,11 @@ export function DoubleArrowIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function LinkArrow(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <path d="M16.0037 9.41421L7.39712 18.0208L5.98291 16.6066L14.5895 8H7.00373V6H18.0037V17H16.0037V9.41421Z"/>
+    </svg>
+  )
+}
--- ui/icons/misc.tsx
+++ ui/icons/misc.tsx
@@ -52,3 +52,21 @@ export function GlobeIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function BookIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <path
+        d="M3 18.5V5C3 3.34315 4.34315 2 6 2H20C20.5523 2 21 2.44772 21 3V21C21 21.5523 20.5523 22 20 22H6.5C4.567 22 3 20.433 3 18.5ZM19 20V17H6.5C5.67157 17 5 17.6716 5 18.5C5 19.3284 5.67157 20 6.5 20H19ZM5 15.3368C5.45463 15.1208 5.9632 15 6.5 15H19V4H6C5.44772 4 5 4.44772 5 5V15.3368Z"></path>
+    </svg>
+  )
+}
+
+export function ComputerIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <path
+        d="M4 5V16H20V5H4ZM2 4.00748C2 3.45107 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44892 22 4.00748V18H2V4.00748ZM1 19H23V21H1V19Z"></path>
+    </svg>
+  )
+}
