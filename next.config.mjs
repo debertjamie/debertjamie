5ec58cc30db3fcc8ccfa8c80c8f34db6ca8aa3e3
debertjamie <debertjamie@outlook.com>
2025-01-27 23:44:32 +0700

    refactor: Add remote URL pattern for image link

--- next.config.mjs
+++ next.config.mjs
@@ -34,6 +34,12 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "play-lh.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "m.media-amazon.com",
         port: "",
         pathname: "/**",
