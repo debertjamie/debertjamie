086210161421f3b25b54860cb0a319a85f4e5358
debertjamie <debertjamie@outlook.com>
2025-02-04 23:24:52 +0700

    feat: Update 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,8 +5,8 @@ export default async function NotFound() {
     <div className="h-screen flex flex-col justify-center items-center gap-y-6">
       <h1 className="text-5xl font-bold">404 Page Not Found</h1>
       <div className="text-lg">
-        <p>What are you even looking for 🤨?</p>
-        <p>This place doesn&#39;t hold any secrets... yet. Check back later.</p>
+        <p>You encountered a page that is in progress, or doesn't exist...</p>
+        <p>Try checking back later for (possibly) some easter eggs 🤔</p>
       </div>
       <Link
         className="block w-fit px-4 py-2 bg-blue-800 text-zinc-100 font-semibold text-lg rounded-lg"
