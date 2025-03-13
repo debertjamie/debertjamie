6b858189dff88d49d1e5905d405a28dbc4ac3a13
debertjamie <debertjamie@outlook.com>
2025-03-13 22:37:31 +0700

    feat: Update intro

--- ui/landing/intro.tsx
+++ ui/landing/intro.tsx
@@ -4,25 +4,17 @@ export function Intro() {
   return (
     <section className="text-xl">
       <p className="text-justify mb-8">
-        Hi! I'm Debert Jamie Chanderson, a student at Universitas Gadjah Mada with a passion in software development
-        and machine learning. I'm also interested in music and public speaking. I mostly spend my free time working on
-        school projects or personal side projects. You can check out my socials and feel free to connect with me on
-        LinkedIn 😄
+        Hi! I'm Debert Jamie Chanderson, a student at Universitas Gadjah Mada with a passion in software engineering
+        and public speaking. I'm also interested in music and photography. I mostly spend my free time working on
+        school projects or personal side projects. I'm pretty active on social media and I love to connect with new
+        people. You can also find me studying or hanging out on campus :D
       </p>
-      <div className="grid sm:grid-cols-2 gap-y-4 gap-x-20 *:w-fit">
-        <Link
-          href="/about"
-          className="inline-block text-cyan-600 dark:text-cyan-500 font-semibold group"
-        >
-          Read More About Me <span className="duration-300 delay-100 group-hover:ml-2">-&gt;</span>
-        </Link>
-        <Link
-          href="/static/Resume_Debert%20Jamie%20C.pdf"
-          className="inline-block text-cyan-600 dark:text-cyan-500 font-semibold group"
-        >
-          Curriculum Vitae <span className="duration-300 delay-100 group-hover:ml-2">-&gt;</span>
-        </Link>
-      </div>
+      <Link
+        href="/about"
+        className="inline-block text-cyan-600 dark:text-cyan-500 font-semibold group"
+      >
+        Read More About Me <span className="duration-300 delay-100 group-hover:ml-2">-&gt;</span>
+      </Link>
     </section>
   );
 }
