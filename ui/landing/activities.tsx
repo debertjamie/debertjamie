8ba74d1442cf451d64ff9b3ec6c3ae2714fa633c
debertjamie <debertjamie@outlook.com>
2025-03-15 15:20:38 +0700

    feat: Custom background for playing and not playing

--- ui/landing/activities.tsx
+++ ui/landing/activities.tsx
@@ -24,7 +24,7 @@ export function Activities() {
       style={{
         backgroundImage: `url('${data?.isPlaying ?
         "https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        : "https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}')`,
+        : "https://images.unsplash.com/photo-1507676184212-d03ab07a01bf?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}')`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "50% 50%",
         backgroundAttachment: "fixed",
