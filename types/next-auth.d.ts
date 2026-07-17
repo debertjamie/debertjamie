56cbcac35579289c9957b914b46f7a7626a96555
debertjamie <debertjamie@outlook.com>
2026-07-17 21:00:25 +0700

    feat(guestbook): Remove Guestbook and components

--- types/next-auth.d.ts
+++
@@ -1,9 +0,0 @@
-import { type DefaultSession } from "@auth/core/types";
-
-declare module "@auth/core/types" {
-  interface Session {
-    user: {
-      isAdmin: boolean;
-    } & DefaultSession["user"];
-  }
-}
