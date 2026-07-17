aa6baffb77b8f88ca4667213025ac3c2554ff788
debertjamie <debertjamie@outlook.com>
2026-07-17 21:13:52 +0700

    feat: Add icons and refactor several lines

--- ui/icons/arrow.tsx
+++ ui/icons/arrow.tsx
@@ -1,25 +1,5 @@
 import type { SVGProps } from "react";
 
-export function DoubleArrowIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="m6 17 5-5-5-5" />
-      <path d="m13 17 5-5-5-5" />
-    </svg>
-  );
-}
-
 export function LinkArrow(props: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
--- ui/icons/index.ts
+++ ui/icons/index.ts
@@ -1,5 +1,4 @@
 export * from "./theme";
-export * from "./logo";
 export * from "./arrow";
 export * from "./socials";
 export * from "./misc";
